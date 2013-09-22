f9aa5b1f10479fcc53b8e9622c56a865c1f08045
Wes Johnson <swj@wesquire.ca>
2013-09-22 17:11:22 -0400

    update requirements

--- package.js
+++ package.js
@@ -3,12 +3,12 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
-    if (typeof api.imply !== "undefined") {
+/*    if (typeof api.imply !== "undefined") {
         api.imply('simple-schema', ['client', 'server']);
-    } else {
+    } else {*/
         //backwards compatibility with pre-0.6.5 meteor
         api.use('simple-schema', ['client', 'server']);
-    }
+//    }
     api.use('underscore', ['client', 'server']);
     api.use('deps', 'client');
     api.add_files(['collection2.js'], ['client', 'server']);
